e8a710bea1db9d04a3d716f31c277c3990d8cda7
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-27 12:56:59 -0800

    working UI with dattcore
    There is now a (functionless) UI that loads dattcore. The UI does not actually
    do anything yet, but it does load and initialize dattcore.

---
+++ react/layout.jsx
@@ -0,0 +1,67 @@
+/**
+ * Layout
+ * =====
+ *
+ * This is the main layout component which frames every page.
+ */
+'use strict'
+let React = require('react')
+let asink = require('asink')
+let yoursVersion = require('../package').version
+
+let Layout = React.createClass({
+  getInitialState: function () {
+    return {
+      dattcoreStatus: 'uninitialized'
+    }
+  },
+
+  componentWillMount: function () {
+    return asink(function *() {
+      let dattcore = this.props.dattcore
+      try {
+        yield dattcore.asyncInitialize()
+        this.setState({
+          dattcoreStatus: 'initialized'
+        })
+        this.monitorDattCore()
+      } catch (err) {
+        this.setState({
+          dattcoreStatus: 'error initializing: ' + err
+        })
+      }
+      yield dattcore.asyncNetworkInitialize()
+    }, this)
+  },
+
+  propTypes: {
+    apptitle: React.PropTypes.string,
+    dattcore: React.PropTypes.object
+  },
+
+  render: function () {
+    let dattcore = this.props.dattcore
+    let dattcoreStatus = this.state.dattcoreStatus
+    return (
+      <div className='container'>
+        <div className='row page-header'>
+          <div className='col-md-12'>
+            <h1>Yours</h1>
+          </div>
+        </div>
+
+        <div className='row page-footer'>
+          <div className='col-md-12'>
+            <div className='version-number'>
+              <p>Yours v{yoursVersion}</p>
+              <p>Status of dattcore: {dattcoreStatus}</p>
+              <p>Datt v{dattcore.version}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Layout
---
+++ server/app.js
@@ -0,0 +1,43 @@
+/**
+ * App Server
+ * ==========
+ *
+ * This server delivers the front-end to web browsers and also runs a proxy to
+ * the blockchain API.
+ */
+'use strict'
+let express = require('express')
+let path = require('path')
+let httpProxy = require('http-proxy')
+let config = require('datt/config')
+
+module.exports.createAppServer = function createAppServer (port) {
+  let app = express()
+
+  // The blockchain API proxy.
+  let apiProxy = httpProxy.createProxyServer()
+  app.all('/blockchain-api/*', (req, res) => {
+    req.url = req.url.replace('/blockchain-api/', '/')
+    apiProxy.web(req, res, {
+      target: config.DATT_BLOCKCHAIN_API_URI,
+      changeOrigin: true
+    })
+  })
+  apiProxy.on('error', (err, req, res) => {
+    if (err) {
+      res.end()
+    }
+  })
+
+  // The front-end is just static HTML, CSS and JS files.
+  app.use(express.static(path.join(__dirname, '../build')))
+
+  let server = app.listen(port, () => {
+    let host = server.address().address
+    let port = server.address().port
+    console.log('View the app at http://%s:%s/', host, port)
+    console.log('Run the mocha browser tests at http://%s:%s/tests.html', host, port)
+  })
+
+  return server
+}
