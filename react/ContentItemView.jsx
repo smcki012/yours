c9849ae9411766cc2d096dbabf0b03d11dfe4213
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-27 23:34:32 -0800

    displaying content correctly kind of

--- react/ContentItemView.jsx
+++ react/ContentItemView.jsx
@@ -3,14 +3,20 @@ let React = require('react')
 let ContentItemView = React.createClass({
   render: function () {
     let contentItem = this.props.contentItem
-    console.log('ContentItemView')
     let dattcore = this.props.dattcore
-    console.log('ContentItemView 2')
+    let DattCore = dattcore.constructor
+    let ContentAuth = DattCore.ContentAuth
+    let contentauth = ContentAuth().fromHex(contentItem.datahex)
+    let content = contentauth.getContent()
+    let url = content.body
+    let name = content.name
+    let title = content.title
+    let pubkey = contentauth.pubkey.toString()
     return (
     <div className='content-item'>
         <button className='btn btn-success'>Endorse</button>
-        <h2><a href='#'>{contentItem.title}</a></h2>
-        <div className='author'>$$$ invested | pubkey | author | date</div>
+        <h2><a href='url'>{content.title}</a></h2>
+        <div className='author'>$$$ invested | {pubkey} | {name} | date</div>
         </div>
     )
   }
