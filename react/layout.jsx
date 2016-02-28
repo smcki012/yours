a0b6618159cb85d2203b208a1f5cd3559c8502f7
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-27 16:26:51 -0800

    routing

--- react/layout.jsx
+++ react/layout.jsx
@@ -8,6 +8,129 @@
 let React = require('react')
 let asink = require('asink')
 let yoursVersion = require('../package').version
+let ReactRouter = require('react-router')
+
+let Link = ReactRouter.Link
+let browserHistory = ReactRouter.browserHistory
+let Route = ReactRouter.Route
+let Router = ReactRouter.Router
+
+let PageContentHot = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <div className='row navigation'>
+          <div className='col-md-12'>
+            <ul className="nav nav-pills">
+              <li role="presentation" className="active"><Link to={`/`}>Hot</Link></li>
+              <li role="presentation"><Link to={`/all/`}>All</Link></li>
+              <li role="presentation"><Link to={`/new/`}>New</Link></li>
+              <li role="presentation"><Link to={`/wallet/`}>Wallet</Link></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='row content-link'>
+          <div className='col-md-12'>
+            <div className='content-item'>
+              <button className='btn btn-success'>Invest $1</button>
+              <h2><a href='#'>A compilation of cat gifs on imgur</a></h2>
+              <div className='author'>$$$ invested | pubkey | author | date</div>
+            </div>
+            <div className='content-item'>
+              <button className='btn btn-success'>Invest $1</button>
+              <h2><a href='#'>An article about how tech both is and isn't in a bubble</a></h2>
+              <div className='author'>$$$ invested | pubkey | author | date</div>
+            </div>
+            <div className='content-item'>
+              <button className='btn btn-success'>Invest $1</button>
+              <h2><a href='#'>An article about how expensive rent in San Francisco is</a></h2>
+              <div className='author'>$$$ invested | pubkey | author | date</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+let PageContentAll = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <div className='row navigation'>
+          <div className='col-md-12'>
+            <ul className="nav nav-pills">
+              <li role="presentation"><Link to={`/`}>Hot</Link></li>
+              <li role="presentation" className="active"><Link to={`/all/`}>All</Link></li>
+              <li role="presentation"><Link to={`/new/`}>New</Link></li>
+              <li role="presentation"><Link to={`/wallet/`}>Wallet</Link></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='row content-link'>
+          <div className='col-md-12'>
+            <div className='content-item'>
+              <button className='btn btn-success'>Invest $1</button>
+              <h2><a href='#'>An article about how tech both is and isn't in a bubble</a></h2>
+              <div className='author'>$$$ invested | pubkey | author | date</div>
+            </div>
+            <div className='content-item'>
+              <button className='btn btn-success'>Invest $1</button>
+              <h2><a href='#'>An article about how expensive rent in San Francisco is</a></h2>
+              <div className='author'>$$$ invested | pubkey | author | date</div>
+            </div>
+            <div className='content-item'>
+              <button className='btn btn-success'>Invest $1</button>
+              <h2><a href='#'>A compilation of cat gifs on imgur</a></h2>
+              <div className='author'>$$$ invested | pubkey | author | date</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+let PageContentNew = React.createClass({
+  render: function () {
+    return (
+
+      <div>
+        <div className='row navigation'>
+          <div className='col-md-12'>
+            <ul className="nav nav-pills">
+              <li role="presentation"><Link to={`/`}>Hot</Link></li>
+              <li role="presentation"><Link to={`/all/`}>All</Link></li>
+              <li role="presentation" className="active"><Link to={`/new/`}>New</Link></li>
+              <li role="presentation"><Link to={`/wallet/`}>Wallet</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+let PageWallet = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <div className='row navigation'>
+          <div className='col-md-12'>
+            <ul className="nav nav-pills">
+              <li role="presentation"><Link to={`/`}>Hot</Link></li>
+              <li role="presentation"><Link to={`/all/`}>All</Link></li>
+              <li role="presentation"><Link to={`/new/`}>New</Link></li>
+              <li role="presentation" className="active"><Link to={`/wallet/`}>Wallet</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
 
 let Layout = React.createClass({
   getInitialState: function () {
@@ -52,46 +175,12 @@ let Layout = React.createClass({
           </div>
         </div>
 
-        <div className='row navigation'>
-          <div className='col-md-12'>
-            <ul className="nav nav-pills">
-              <li role="presentation" className="active"><a href="#">Hot</a></li>
-              <li role="presentation"><a href="#">All</a></li>
-              <li role="presentation"><a href="#">New</a></li>
-              <li role="presentation"><a href="#">Wallet</a></li>
-            </ul>
-          </div>
-        </div>
-
-        <div className='row content-link'>
-          <div className='col-md-12'>
-            <div className='content-item'>
-              <button className='btn btn-success'>Invest $1</button>
-              <h2><a href='#'>A compilation of cat gifs on imgur</a></h2>
-              <div className='author'>$$$ invested | pubkey | author | date</div>
-            </div>
-          </div>
-        </div>
-
-        <div className='row content-link'>
-          <div className='col-md-12'>
-            <div className='content-item'>
-              <button className='btn btn-success'>Invest $1</button>
-              <h2><a href='#'>An article about how tech both is and isn't in a bubble</a></h2>
-              <div className='author'>$$$ invested | pubkey | author | date</div>
-            </div>
-          </div>
-        </div>
-
-        <div className='row content-link'>
-          <div className='col-md-12'>
-            <div className='content-item'>
-              <button className='btn btn-success'>Invest $1</button>
-              <h2><a href='#'>An article about how expensive rent in San Francisco is</a></h2>
-              <div className='author'>$$$ invested | pubkey | author | date</div>
-            </div>
-          </div>
-        </div>
+        <Router history={browserHistory}>
+          <Route path="/" component={PageContentHot}/>
+          <Route path="/all" component={PageContentAll}/>
+          <Route path="/new" component={PageContentNew}/>
+          <Route path="/wallet" component={PageWallet}/>
+        </Router>
 
         <div className='row page-footer'>
           <div className='col-md-12'>
