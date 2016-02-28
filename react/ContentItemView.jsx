fb55d31ae60df3c9e7adc228c5da19a786ea6c86
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-28 00:12:50 -0800

    clicking button to make bitcoin tx works

--- react/ContentItemView.jsx
+++ react/ContentItemView.jsx
@@ -1,6 +1,20 @@
 let React = require('react')
+let asink = require('asink')
 
 let ContentItemView = React.createClass({
+  handleSubmit: function (el) {
+    return asink(function *() {
+      console.log('submit was called')
+      el.preventDefault()
+      let contentItem = this.props.contentItem
+      let DattCore = dattcore.constructor
+    let ContentAuth = DattCore.ContentAuth
+      let contentauth = ContentAuth().fromHex(contentItem.datahex)
+      let address = contentauth.address
+      yield dattcore.asyncBuildSignAndSendTransaction(address, 5000 * 1e2)
+    }, this)
+  },
+
   render: function () {
     let contentItem = this.props.contentItem
     let dattcore = this.props.dattcore
@@ -14,9 +28,10 @@ let ContentItemView = React.createClass({
     let title = content.title
     let label = content.label
     let pubkey = contentauth.pubkey.toString()
+    let address = contentauth.address
     return (
     <div className='content-item'>
-        <button className='btn btn-success'>Endorse</button>
+        <button className='btn btn-success' onClick={this.handleSubmit}>Endorse</button>
         <h2><a href={url}>{title}</a></h2>
         <div className='author'>$$$ invested | {pubkey} | {name} | date</div>
         </div>
