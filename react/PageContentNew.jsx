f6655f85028fb5bde5587dd5dc1b34466816d8dc
Ryan X. Charles <ryanxcharles@gmail.com>
2016-02-27 22:46:09 -0800

    fix (some) js standard issues

---
+++ react/PageContentNew.jsx
@@ -0,0 +1,140 @@
+let React = require('react')
+let asink = require('asink')
+let ReactRouter = require('react-router')
+let Link = ReactRouter.Link
+
+let PageContentNew = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <div className='row navigation'>
+          <div className='col-md-12'>
+            <ul className='nav nav-pills'>
+              <li role='presentation'><Link to='/'>Hot</Link></li>
+              <li role='presentation'><Link to='/all'>All</Link></li>
+              <li role='presentation' className='active'><Link to='/new'>New</Link></li>
+              <li role='presentation'><Link to='/wallet'>Wallet</Link></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='row page-content-new'>
+          <div className='col-md-12'>
+            <FormNewContent dattcore={this.props.dattcore}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+let FormNewContent = React.createClass({
+  getInitialState: function () {
+    return {
+      inputTitle: '',
+      inputLabel: '',
+      inputName: '',
+      inputBody: ''
+    }
+  },
+
+  propTypes: {
+    dattcore: React.PropTypes.object
+  },
+
+  handleTitleChange: function (el) {
+    this.setState({
+      inputTitle: el.target.value
+    })
+  },
+
+  handleNameChange: function (el) {
+    this.setState({
+      inputName: el.target.value
+    })
+  },
+
+  handleLabelChange: function (el) {
+    this.setState({
+      inputLabel: el.target.value
+    })
+  },
+
+  handleBodyChange: function (el) {
+    this.setState({
+      inputBody: el.target.value
+    })
+  },
+
+  handleSubmit: function (el) {
+    return asink(function * () {
+      el.preventDefault()
+      let title = this.state.inputTitle
+      let label = this.state.inputLabel
+      let body = this.state.inputBody
+      let name = this.state.inputName
+      let dattcore = this.props.dattcore
+
+      // create, but do not post or send the new content
+      let contentauth = yield dattcore.asyncNewContentAuth(title, label, body)
+      console.log(contentauth.toBuffer().toString('hex'))
+      yield dattcore.asyncSetUserName(name)
+
+      this.setState(this.getInitialState())
+    }, this)
+  },
+
+  render: function () {
+    return (
+      <div className='row'>
+        <div className='col-md-8 col-md-offset-2'>
+          <div className='author-new-content well'>
+            <form>
+              <div className='form-group'>
+                <label htmlFor='inputTitle'>
+                  Title
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='inputTitle'
+                  placeholder='Title'
+                  onChange={this.handleTitleChange}
+                  value={this.state.inputTitle} />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='inputBody'>
+                  URL
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='inputBody'
+                  placeholder='URL'
+                  onChange={this.handleBodyChange}
+                  value={this.state.inputBody} />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='inputBody'>
+                  Your Name
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='inputBody'
+                  placeholder='Your Name'
+                  onChange={this.handleNameChange}
+                  value={this.state.inputName} />
+              </div>
+              <button type='submit' className='btn btn-default' onClick={this.handleSubmit}>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = PageContentNew
--- react/layout.jsx
+++ react/layout.jsx
@@ -15,7 +15,6 @@ let browserHistory = ReactRouter.browserHistory
 let Route = ReactRouter.Route
 let Router = ReactRouter.Router
 
-let NavigationBar = require('./NavigationBar.jsx')
 let ContentItemView = require('./ContentItemView.jsx')
 let MultiContentItemView = require('./MultiContentItemView.jsx')
 
@@ -37,142 +36,9 @@ let PageContentHot = React.createClass({
   }
 })
 
+let PageContentNew = require('./PageContentNew.jsx')
+
 let PageWallet = require('./PageWallet.jsx')
-
-let PageContentNew = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <div className='row navigation'>
-          <div className='col-md-12'>
-            <ul className="nav nav-pills">
-              <li role="presentation"><Link to={`/`}>Hot</Link></li>
-              <li role="presentation"><Link to={`/all/`}>All</Link></li>
-              <li role="presentation" className="active"><Link to={`/new/`}>New</Link></li>
-              <li role="presentation"><Link to={`/wallet/`}>Wallet</Link></li>
-            </ul>
-          </div>
-        </div>
-
-        <div className='row page-content-new'>
-          <div className='col-md-12'>
-            <FormNewContent dattcore={this.props.dattcore}/>
-          </div>
-        </div>
-      </div>
-    )
-  }
-})
-
-let FormNewContent = React.createClass({
-  getInitialState: function () {
-    return {
-      inputTitle: '',
-      inputLabel: '',
-      inputName: '',
-      inputBody: ''
-    }
-  },
-
-  propTypes: {
-    dattcore: React.PropTypes.object
-  },
-
-  handleTitleChange: function (el) {
-    this.setState({
-      inputTitle: el.target.value
-    })
-  },
-
-  handleNameChange: function (el) {
-    this.setState({
-      inputName: el.target.value
-    })
-  },
-
-  handleLabelChange: function (el) {
-    this.setState({
-      inputLabel: el.target.value
-    })
-  },
-
-  handleBodyChange: function (el) {
-    this.setState({
-      inputBody: el.target.value
-    })
-  },
-
-  handleSubmit: function (el) {
-    return asink(function * () {
-      el.preventDefault()
-      let title = this.state.inputTitle
-      let label = this.state.inputLabel
-      let body = this.state.inputBody
-      let name = this.state.inputName
-      let dattcore = this.props.dattcore
-
-      // create, but do not post or send the new content
-      let contentauth = yield dattcore.asyncNewContentAuth(title, label, body)
-      yield dattcore.asyncSetUserName(name)
-
-      this.setState(this.getInitialState())
-    }, this)
-  },
-
-  render: function () {
-    return (
-      <div className='row'>
-        <div className='col-md-8 col-md-offset-2'>
-          <div className='author-new-content well'>
-            <form>
-              <div className='form-group'>
-                <label htmlFor='inputTitle'>
-                  Title
-                </label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='inputTitle'
-                  placeholder='Title'
-                  onChange={this.handleTitleChange}
-                  value={this.state.inputTitle} />
-              </div>
-              <div className='form-group'>
-                <label htmlFor='inputBody'>
-                  URL
-                </label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='inputBody'
-                  placeholder='URL'
-                  onChange={this.handleBodyChange}
-                  value={this.state.inputBody} />
-              </div>
-              <div className='form-group'>
-                <label htmlFor='inputBody'>
-                  Your Name
-                </label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='inputBody'
-                  placeholder='Your Name'
-                  onChange={this.handleNameChange}
-                  value={this.state.inputName} />
-              </div>
-              <button type='submit' className='btn btn-default' onClick={this.handleSubmit}>
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
-})
-
-let BoxBitcoin = require('./BoxBitcoin.jsx')
 
 let Layout = React.createClass({
   getInitialState: function () {
